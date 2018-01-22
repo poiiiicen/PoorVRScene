e4ea41433646f46331c7c55c691cce6bdeaecc5a
poiiiicen <poiiiicen@gmail.com>
2018-01-22 16:43:10 +0800

    Finish draw triangle

--- app/src/main/java/com/example/poiii/ztyt/MyRender.kt
+++ app/src/main/java/com/example/poiii/ztyt/MyRender.kt
@@ -1,7 +1,10 @@
 package com.example.poiii.ztyt
 
+import android.opengl.GLES20
 import android.opengl.GLSurfaceView
-import android.opengl.GLU
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -9,27 +12,48 @@ import javax.microedition.khronos.opengles.GL10
  * Created by poiii on 2018/1/15.
  */
 class MyRender(private val activity: MainActivity) : GLSurfaceView.Renderer {
-    private val VERTEX_SHADER: String = Utils.loadShader(activity, R.raw.vertex)
-    private val FRAGMENT_SHADER: String = Utils.loadShader(activity, R.raw.fragment)
+    private val VERTEX_SHADER = Utils.loadShaderCode(activity, R.raw.vertex)
+    private val FRAGMENT_SHADER = Utils.loadShaderCode(activity, R.raw.fragment)
+    private var mProgram = 0
+    private lateinit var vertexBuffer: FloatBuffer
+    private var vertexShader = 0
+    private var fragShader = 0
+
+    private val triangleCoords = floatArrayOf(0.5f, 0.5f, 0.0f, // top
+            -0.5f, -0.5f, 0.0f, // bottom left
+            0.5f, -0.5f, 0.0f  // bottom right
+    )
+    private val vertexCount = 12;
+
+    val color = floatArrayOf(1.0f, 1.0f, 1.0f, 1.0f)
 
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f)
+        val bb = ByteBuffer.allocateDirect(triangleCoords.size * 4)
+        bb.order(ByteOrder.nativeOrder())
+        vertexBuffer = bb.asFloatBuffer()
+        vertexBuffer.put(triangleCoords)
+        vertexBuffer.position(0)
+        vertexShader = Utils.loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER)
+        fragShader = Utils.loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER)
+        mProgram = GLES20.glCreateProgram()
+        GLES20.glAttachShader(mProgram, vertexShader)
+        GLES20.glAttachShader(mProgram, fragShader)
+        GLES20.glLinkProgram(mProgram)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         gl!!.glViewport(0, 0, width, height)
-
-        val ratio = width.toFloat() / height.toFloat()
-        gl.glMatrixMode(GL10.GL_PROJECTION)
-        gl.glLoadIdentity()
-        gl.glFrustumf(-ratio, ratio, -1.0f, 1.0f, 3.0f, 7.0f)
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onDrawFrame(gl: GL10?) {
-        gl!!.glMatrixMode(GL10.GL_MODELVIEW)
-        gl.glLoadIdentity()
-        GLU.gluLookAt(gl, 0.0f, 0.0f, -5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f)
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        GLES20.glUseProgram(mProgram)
+        val mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition")
+        GLES20.glEnableVertexAttribArray(mPositionHandle)
+        GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false, 12, vertexBuffer)
+        val mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor")
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
+        GLES20.glDisableVertexAttribArray(mPositionHandle)
     }
 }
--- app/src/main/java/com/example/poiii/ztyt/Utils.kt
+++ app/src/main/java/com/example/poiii/ztyt/Utils.kt
@@ -3,13 +3,17 @@ package com.example.poiii.ztyt
 import android.content.Context
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import android.opengl.GLES20
+import android.content.ContentValues.TAG
+import android.util.Log
+
 
 /**
  * Created by poiii on 2018/1/15.
  */
 object Utils {
     @JvmStatic
-    fun loadShader(context: Context, id: Int): String {
+    fun loadShaderCode(context: Context, id: Int): String {
         val builder = StringBuilder()
         val inputStream = context.resources.openRawResource(id)
         val reader = BufferedReader(InputStreamReader(inputStream))
@@ -21,5 +25,23 @@ object Utils {
         }
         reader.close()
         return builder.toString()
+    }
+
+    @JvmStatic
+    fun loadShader(shaderType: Int, source: String): Int {
+        var shader = GLES20.glCreateShader(shaderType)
+        if (0 != shader) {
+            GLES20.glShaderSource(shader, source)
+            GLES20.glCompileShader(shader)
+            val compiled = IntArray(1)
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0)
+            if (compiled[0] == 0) {
+                Log.e(TAG, "Could not compile shader:" + shaderType)
+                Log.e(TAG, "GLES20 Error:" + GLES20.glGetShaderInfoLog(shader))
+                GLES20.glDeleteShader(shader)
+                shader = 0
+            }
+        }
+        return shader
     }
 }
